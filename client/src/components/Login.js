ff874726105773a9973fdabf428e7b225876accd
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-10-17 20:22:03 +0530

    frontend for login

---
+++ client/src/components/Login.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validateForm = () => {
+    let formErrors = {};
+    if (!formData.email) {
+      formErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      formErrors.email = "Email is invalid";
+    }
+
+    if (!formData.password) {
+      formErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      formErrors.password = "Password must be at least 6 characters";
+    }
+    return formErrors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length === 0) {
+      try {
+        // Perform login logic here
+        const response = await axios.post(
+          "http://localhost:5000/api/auth/login",
+          formData,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log("Login successful:", response.data);
+
+        // Store the token in localStorage if the response includes a token
+        const { token } = response.data;
+        localStorage.setItem("token", token);
+      } catch (error) {
+        console.log("Login failed:", error.response.data.message);
+      }
+    } else {
+      setErrors(formErrors);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
