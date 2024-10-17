c7e27b5162c349e4097ea18b5f3b096a67aae681
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-10-17 14:00:38 +0530

    Created server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// MongoDB Connection
+const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.error(err));
