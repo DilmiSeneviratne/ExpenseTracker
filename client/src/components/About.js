303c2a50d532f158516d6905585b188632e407fb
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-11-13 18:37:20 +0530

    Changed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import AboutBackgroundImage from '../Assests/about-background-image.png';
+import { BsFillPlayCircleFill } from 'react-icons/bs';
 const About = () => {
   return (
-    <div>
+    <div className="about-section-container">
       
     </div>
-  )
+  );
 }
 
 export default About
