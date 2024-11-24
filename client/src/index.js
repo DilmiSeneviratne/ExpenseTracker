9d9386217b6857340e430ffeb0a96dc7acc5995b
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-11-24 14:56:03 +0530

    updated routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
-
+// Create a root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App inside the Router
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
