c069426b2dc48b23dc56189babdfb3f36d26fdd3
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-11-28 22:41:51 +0530

    Created expenses component

---
+++ client/src/components/Expense.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Expense = () => {
+  return (
+    <>
+      <div class="p-4 ">
+        <div class="grid grid-cols-3 gap-4 mb-4">Expense Page</div>
+      </div>
+      
+    </>
+  );
+};
+
+export default Expense;
