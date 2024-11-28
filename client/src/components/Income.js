7b8aa48a87b3501b327a8b61d3a7243e72dabf1f
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-11-28 22:42:10 +0530

    Created income component

---
+++ client/src/components/Income.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Income = () => {
+  return (
+    <>
+      <div class="p-4 ">
+        <div class="grid grid-cols-3 gap-4 mb-4">
+          Income Page
+        </div>
+        
+      </div>
+      
+    </>
+  );
+};
+
+export default Income;
