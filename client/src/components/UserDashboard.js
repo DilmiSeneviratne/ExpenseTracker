544d962b843f0c8971719b49ed41f49f8598dcd9
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-11-28 22:42:47 +0530

    Changed to include navlinks and outlets

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from "./Footer";
-
+import { Outlet, NavLink } from 'react-router-dom';
 const UserDashboard = () => {
   return (
     <>
@@ -12,9 +12,13 @@ const UserDashboard = () => {
         <div class="h-full px-3 py-4 overflow-y-auto bg-emerald-500 dark:bg-emerald-800">
           <ul class="space-y-2 font-medium">
             <li>
-              <a
-                href="#"
-                class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-emerald-600 dark:hover:bg-emerald-700 group"
+              <NavLink
+                to="dashboard"
+                className={({ isActive }) =>
+                  `flex items-center p-2 rounded-lg text-white ${
+                    isActive ? "bg-emerald-600" : ""
+                  }`
+                }
               >
                 <svg
                   class="w-5 h-5 text-white transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
@@ -27,12 +31,16 @@ const UserDashboard = () => {
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
                 <span class="ms-3">Dashboard</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a
-                href="#"
-                class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-emerald-600 dark:hover:bg-emerald-700 group"
+              <NavLink
+                to="income"
+                className={({ isActive }) =>
+                  `flex items-center p-2 rounded-lg text-white ${
+                    isActive ? "bg-emerald-600" : ""
+                  }`
+                }
               >
                 <svg
                   class="w-5 h-5 text-white transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
@@ -44,29 +52,38 @@ const UserDashboard = () => {
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                 </svg>
                 <span class="flex-1 ms-3 whitespace-nowrap">Income</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a
-                href="#"
-                class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-emerald-600 dark:hover:bg-emerald-700 group"
+              <NavLink
+                to="expense"
+                className={({ isActive }) =>
+                  `flex items-center p-2 rounded-lg text-white ${
+                    isActive ? "bg-emerald-600" : ""
+                  }`
+                }
               >
-                <svg
-                  class="w-5 h-5 text-white transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-                </svg>
-                <span class="flex-1 ms-3 whitespace-nowrap">Expense</span>
-              </a>
+                  <svg
+                    class="w-5 h-5 text-white transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                  </svg>
+                  <span class="flex-1 ms-3 whitespace-nowrap">Expense</span>
+                
+              </NavLink>
             </li>
             <li>
-              <a
-                href="#"
-                class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-emerald-600 dark:hover:bg-emerald-700 group"
+              <NavLink
+                to="profile"
+                className={({ isActive }) =>
+                  `flex items-center p-2 rounded-lg text-white ${
+                    isActive ? "bg-emerald-600" : ""
+                  }`
+                }
               >
                 <svg
                   class="w-5 h-5 text-white transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
@@ -78,7 +95,7 @@ const UserDashboard = () => {
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                 </svg>
                 <span class="flex-1 ms-3 whitespace-nowrap">Profile</span>
-              </a>
+              </NavLink>
             </li>
             <li>
               <a
@@ -102,57 +119,9 @@ const UserDashboard = () => {
       </aside>
 
       <div class="p-4 sm:ml-64">
-        <div class="p-4 ">
-          <div class="grid grid-cols-3 gap-4 mb-4">
-            <div class="flex items-center justify-center h-24 rounded-2xl bg-emerald-100 dark:bg-gray-800">
-              <p class="text-2xl text-gray-700 dark:text-gray-900 pr-5">
-                Total Income
-              </p>
-              <p class="text-2xl text-gray-700 dark:text-gray-900">$4000</p>
-            </div>
-            <div class="flex items-center justify-center h-24 rounded-2xl bg-emerald-100 dark:bg-gray-800">
-              <p class="text-2xl  text-gray-700 dark:text-gray-900 pr-5">
-                Total Expenses
-              </p>
-              <p class="text-2xl text-gray-700 dark:text-gray-900">$4000</p>
-            </div>
-            <div class="flex items-center justify-center h-24 rounded-2xl bg-emerald-100 dark:bg-gray-800">
-              <p class="text-2xl  text-gray-700 dark:text-gray-900 pr-5">
-                Total Profit
-              </p>
-              <p class="text-2xl text-gray-700 dark:text-gray-900">$4000</p>
-            </div>
-          </div>
-          <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
-              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Teal to Lime
-              </span>
-            </button>
-            <p class="text-2xl text-gray-400 dark:text-gray-500">Categories</p>
-          </div>
-
-          <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              Summary Charts Bar
-            </p>
-          </div>
-          <div class="grid grid-cols-2 gap-4">
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500"></p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500"></p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500"></p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500"></p>
-            </div>
-          </div>
-        </div>
+        <Outlet />
       </div>
+
       <Footer />
     </>
   );
